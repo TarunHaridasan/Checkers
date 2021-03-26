454cfa67b51f9a5e5fadb91129c659c9a0528a47
TarunHaridasan <tarunharidasan2003@gmail.com>
2021-03-25 20:23:46 -0400

    made class method static

--- src/Screen.java
+++ src/Screen.java
@@ -1,11 +1,11 @@
 import java.io.*;
 public class Screen {
     //Declare properties
-    final int CELLSPACING = 3; //Must be an odd number of program will be fucked
-    final int SIDELENGTH = (CELLSPACING*8)+9;
+    final static int CELLSPACING = 3; //Must be an odd number or printing will be broken
+    final static int SIDELENGTH = (CELLSPACING*8)+9;
 
     //This method prints the checker board onto the console window with the right formatting
-    public void printBoard(String[][] board) {
+    public static void printBoard(String[][] board) {
         for(int i=0; i<SIDELENGTH; i++) {
             for (int j=0; j<SIDELENGTH; j++) {
                 if (i%(CELLSPACING+1)==0) {
@@ -30,20 +30,20 @@ public class Screen {
     }
 
     //This command clears the console window (It adds lots of spaces)
-    public void clear(int space) {
+    public static void clear(int space) {
         for (int i=0; i<space; i++) {
             System.out.println();
         }
     }
 
     //This command clears the console window and then reprints the board to the screen
-    public void refresh(String[][] board, int space) {
+    public static void refresh(String[][] board, int space) {
         clear(space);
         printBoard(board);
     }
 
     //This command reads from ASCII file and prints it
-    public void printFromFile(String fp) throws IOException {
+    public static void printFromFile(String fp) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(fp));
         String line = br.readLine();
         while (line!=null) {
