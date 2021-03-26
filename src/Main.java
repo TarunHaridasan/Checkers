216a5ea057c3c47ba8a2a61252ecd76344daaaf3
Jason <jasonsu894@gmail.com>
2021-03-25 20:46:21 -0400

    Fixes.

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //Testing
-        String[][] board = {
-            {"o", "O", "x", "X", "X", "O", "X", "O"},
-            {"X", "O", "X", "O", "X", "O", "X", "O"},
-            {"X", "O", "X", "O", "X", "O", "X", "O"},
-            {"X", "O", "X", "O", "X", "O", "X", "O"},
-            {"X", "O", "X", "O", "X", "O", "X", "O"},
-            {"X", "O", "X", "O", "X", "O", "X", "O"},
-            {"X", "O", "X", "O", "X", "O", "X", "O"},
-            {"X", "O", "X", "O", "X", "O", "X", "O"}
-        };
-        Screen screen = new Screen();
 
-        //Print the board
-        screen.printBoard(board);
-        //screen.refresh(board, 20);
-
-        //Print ASCII Art
-        //screen.printFromFile("./art/test.txt");
     }
 }
