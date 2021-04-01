8855af5455d6f6ac6838e071e1d0ca19a2a2784a
Jason <jasonsu894@gmail.com>
2021-04-01 16:07:29 -0400

    Added exceptions.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import com.rits.cloning.Cloner;
+
+import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         //Initializing the title screen.
         TitleScreen.initialize();
         /*
